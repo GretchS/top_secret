026310d94d13e1799a0f3a301bac17f9a9bda86d
GretchS <gretchen.scott@hotmail.com>
2017-10-26 11:53:30 +1100

    Extract out company from user's email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def domain 
+    # email will equal sue@spacex.com, then pull out spacex.com
+    email.split('@').last
+    #could use rspec to validate this
+  end
+
+  def company
+    Company.find_by(domain: self.domain)
+  end
+
 end
