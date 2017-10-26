661e925e874d88a33f51a422b32c40251f14fa5c
GretchS <gretchen.scott@hotmail.com>
2017-10-26 11:37:14 +1100

    Added documents index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  #set the home page as documents and redirect if someone types /documents in the turl
+  root "documents#index"
+  resources :documents, only: [:show]
+  get '/documents' => redirect('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
